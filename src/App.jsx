b6497da8ecd50b6bdae01d7a146f5a47bd37177a
Harshkr007 <harshkr1890@gmail.com>
2024-07-11 01:46:41 +0530

    new project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  // import.meta.env.VITE_APPWRITE_URL
+  return (
+    <>
+      <div>A blog app wih appwrite</div>
+    </>
+  );
+}
+
+export default App;
